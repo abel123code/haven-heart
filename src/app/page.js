841c7570f9469c1603e8a1ad3c2bd6cf538a6e7e
Abel Lee <binghengabellee@gmail.com>
2025-02-12 13:48:17 +0800

    update error for npm run build 3

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,7 @@ export default function Home() {
           </div>
           <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
             <Image
-              src="/participants.avif"
+              src="/images/participants.avif"
               alt="Mental health workshop illustration"
               width={600}
               height={400}
